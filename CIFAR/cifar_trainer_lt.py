20fe1366bec19904dc8ba7cff36dcf803517a938
Chumsy0725 <chamuditha.cj@gmail.com>
2021-06-19 23:34:16 +0530

    minor change in batch size

---
+++ CIFAR/cifar_trainer_lt.py
@@ -0,0 +1,224 @@
+import argparse
+import os
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+from model import resnet32
+from dataset import CIFAR10LTNPZDataset
+from utils import AverageMeter, save_checkpoint, accuracy, class_accuracy
+
+parser = argparse.ArgumentParser(
+    description='PyTorch implementation of the paper: Long-tail Learning via Logit Adjustment'
+)
+
+parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number ')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N', help='mini-batch size (default: 128)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=50, type=int,
+                    metavar='N', help='print frequency (default: 50)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--save-dir', dest='save_dir',
+                    help='The directory used to save the trained models',
+                    default='save_temp', type=str)
+parser.add_argument('--save-every', dest='save_every',
+                    help='Saves checkpoints at every specified number of epochs',
+                    type=int, default=10)
+
+best_acc = 0
+args = parser.parse_args()
+
+
+def main():
+    global args, best_acc
+
+    # Check the save_dir exists or not
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    model = torch.nn.DataParallel(resnet32())
+    model.cuda()
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            model.load_state_dict(checkpoint['state_dict'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.evaluate, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    # CIFAR-10 dataset
+    train_dataset = CIFAR10LTNPZDataset(root='data',
+                                        train=True,
+                                        transform=transforms.Compose([
+                                            transforms.RandomHorizontalFlip(),
+                                            transforms.RandomCrop(32, 4) ]))
+
+    test_dataset = CIFAR10LTNPZDataset(root='data',
+                                       train=False)
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=args.batch_size,
+                                               shuffle=True)
+
+    val_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                             batch_size=args.batch_size,
+                                             shuffle=False)
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(),
+                                args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                        milestones=[160, 180],
+                                                        last_epoch=args.start_epoch - 1)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion)
+        return
+
+    for epoch in range(args.start_epoch, args.epochs):
+
+        # train for one epoch
+        print('current lr {:.5e}'.format(optimizer.param_groups[0]['lr']))
+        train(train_loader, model, criterion, optimizer, epoch)
+        lr_scheduler.step()
+
+        # evaluate on validation set
+        acc = validate(val_loader, model, criterion)
+
+        # remember best prec@1 and save checkpoint
+        is_best = acc > best_acc
+        best_acc = max(acc, best_acc)
+
+        if epoch > 0 and epoch % args.save_every == 0:
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'best_acc': best_acc,
+            }, is_best, filename=os.path.join(args.save_dir, 'checkpoint.th'))
+
+        save_checkpoint({
+            'state_dict': model.state_dict(),
+            'best_acc': best_acc,
+        }, is_best, filename=os.path.join(args.save_dir, 'model.th'))
+
+    class_accuracy(val_loader, model)
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    """ Run one train epoch """
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    accuracies = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (inputs, target) in enumerate(train_loader):
+        target = target.cuda()
+        input_var = inputs.cuda()
+        target_var = target
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        output = output.float()
+        loss = loss.float()
+        # measure accuracy and record loss
+        acc = accuracy(output.data, target)
+        losses.update(loss.item(), inputs.size(0))
+        accuracies.update(acc, inputs.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Training Loss | {loss.avg:.4f} \t'
+                  'Training Accuracy | {accuracies.avg:.3f} '.format(epoch, i, len(train_loader),
+                                                                     batch_time=batch_time, loss=losses,
+                                                                     accuracies=accuracies))
+
+
+def validate(val_loader, model, criterion):
+    """ Run evaluation """
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    accuracies = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    with torch.no_grad():
+        for i, (inputs, target) in enumerate(val_loader):
+            target = target.cuda()
+            input_var = inputs.cuda()
+            target_var = target.cuda()
+
+            # compute output
+            output = model(input_var)
+            loss = criterion(output, target_var)
+
+            output = output.float()
+            loss = loss.float()
+
+            # measure accuracy and record loss
+            acc = accuracy(output.data, target)
+            losses.update(loss.item(), inputs.size(0))
+            accuracies.update(acc, inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+        print('Time | {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Validation Loss | {loss.avg:.4f}\t'
+              'Validation Accuracy |  {accuracies.avg:.3f}'.format(batch_time=batch_time, loss=losses,
+                                                                   accuracies=accuracies))
+    return accuracies.avg
+
+
+if __name__ == '__main__':
+    main()
