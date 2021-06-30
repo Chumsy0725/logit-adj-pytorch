59e08f96274b2e498e6d4fd0ff83b6d102b65c87
msutharsan <msuthasan16@gmail.com>
2021-06-30 13:43:23 +0530

    debug cifar100 class size in classs acuracy

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ from torch.utils.data import DataLoader
 from dataset.utils import DATASET_MAPPINGS
 from dataset.transforms import TRAIN_TRANSFORMS, TEST_TRANSFORMS
 
-classes_10 = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -40,9 +38,10 @@ def accuracy(outputs, labels):
 def class_accuracy(test_loader, model, args):
     """ Computes the accuracy for each class"""
     classes = args.class_names
+    num_class = len(args.class_names)
     with torch.no_grad():
-        n_class_correct = [0 for i in range(10)]
-        n_class_samples = [0 for i in range(10)]
+        n_class_correct = [0 for _ in range(num_class)]
+        n_class_samples = [0 for _ in range(num_class)]
         for images, labels in test_loader:
             images = images.to(args.device)
             labels = labels.to(args.device)
