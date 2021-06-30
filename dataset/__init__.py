114b9858534e17d4072bcc590731741f49fd08ed
msutharsan <msuthasan16@gmail.com>
2021-06-30 06:10:45 +0530

    debug

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -107,7 +107,7 @@ class CIFAR10LTNPZDataset(_CIFARLTNPZDataset):
 
     def get_epoch(self):
         # 12406 10000
-        return 1  # 1241
+        return 1241
 
     def get_scheduler(self):
         return [604, 926, 1128]
