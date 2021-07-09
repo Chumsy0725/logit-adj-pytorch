0c6e2bb178c9be37fb81ea580c49a2ade4de455b
Chumsy0725 <chamuditha.cj@gmail.com>
2021-07-09 22:06:16 +0530

    code clean up

--- config.py
+++ config.py
@@ -15,9 +15,7 @@ def get_arguments():
     parser.add_argument('--lr', default=0.1, type=float, help='initial learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--weight-decay', default=1e-4, type=float, help='weight decay (default: 1e-4)')
-
     parser.add_argument('--log_val', help='compute val acc', type=int, default=10)
-
     parser.add_argument('--logit_adj_post', help='adjust logits post hoc', type=int, default=0, choices=[0, 1])
     parser.add_argument('--tro_post_range', help='check diffrent val of tro in post hoc', type=list,
                         default=[0.25, 0.5, 0.75, 1, 1.5, 2])
